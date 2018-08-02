0ae3b71d398cbc9b30acb755dba7c77fb8f79686
Wojtek Bojarczuk <wojtekboj@gmail.com>
2018-08-02 17:50:18 +0200

    Modification project files

--- client/MessageForm.js
+++ client/MessageForm.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 
+import styles from './MessageForm.css';
+
 class MessageForm extends Component {
     constructor(props) {
         super(props);
--- client/MessageList.js
+++ client/MessageList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import styles from './MessageList.css';
 
 const Message = props => (
